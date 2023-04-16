dfa2d73577d25222876d79af116496d3ce320169
Gustavo Arruda <70242475+gugalxp@users.noreply.github.com>
2023-04-16 16:11:20 -0300

    auth database

--- src/config/database.js
+++ src/config/database.js
@@ -18,4 +18,22 @@ module.exports = {
 }
 
 // CONFIG BANCO DEPLOY
-s
+
+// module.exports = {
+//    dialect: 'postgres', 
+//    host: 'localhost',
+//    username: 'postgres',
+//    password: 12345,
+//    database: 'startpn',
+//    port: 5432,
+
+//    define: {
+//       timestamps: true,
+//    },
+   
+//    pool: {
+//       max: 5,
+//       min: 0,
+//       idle: 10000
+//    }
+// }
